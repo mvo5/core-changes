74894772ccdae6fb45b2436814d14b0510e9d5ae
Michael Vogt <mvo@ubuntu.com>
2017-12-19 12:48:40 +0100

    silence cron.sh some more

--- bin/cron.sh
+++ bin/cron.sh
@@ -15,7 +15,7 @@ for ch in stable candidate beta edge; do
 
     # ensure we have an archive of snaps available for the changes
     # generation
-    (cd "$ARCHIVE" && snap download "--$ch" core 2>/dev/null)
+    (cd "$ARCHIVE" && snap download "--$ch" core >/dev/null 2>&1)
 
     # generate the changes
     CHANGES="$OUTPUT/changes-$ch.txt"
@@ -26,7 +26,7 @@ for ch in stable candidate beta edge; do
 
     # move things into place, don't change timestamp unless there are changes
     # so that if-modified-since etc works
-    if [ ! -e $CHANGES ] || ! cmp <(grep -v "Generated on" "${CHANGES}".tmp) <(grep -v "Generated on" "$CHANGES") 2>/dev/null; then
+    if [ ! -e $CHANGES ] || ! cmp <(grep -v "Generated on" "${CHANGES}".tmp) <(grep -v "Generated on" "$CHANGES") >/dev/null 2>&1; then
         mv "${CHANGES}.tmp" "$CHANGES"
     fi
     rm -f "${CHANGES}".tmp
