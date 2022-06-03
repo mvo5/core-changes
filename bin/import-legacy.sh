15e83d5df6ede8345d157d9bfcb32c46f1df9c4c
Michael Vogt <mvo@ubuntu.com>
2022-06-03 09:28:02 +0200

    import-legacy: tweak

--- bin/import-legacy.sh
+++ bin/import-legacy.sh
@@ -2,15 +2,17 @@
 
 set -e
 
+LEGACY_DIR=~/core-changes
+
 BASE=$(readlink -f "$(dirname "$0")/..")
 PATH="$BASE/bin":$PATH
 
 
 for snap in core22 core20 core core18 core16; do
     for ch in edge beta candidate stable; do
-        ARCHIVE="$BASE"/archive-"$snap"-"$ch"
+        ARCHIVE="$LEGACY_DIR"/archive-"$snap"-"$ch"
 
-        gen-core-changes.py --import-to-db \
+        gen-core-changes.py --import-to-db -v \
                             --archive-dir "$ARCHIVE" \
                             --track "$ch" \
                             --snap "$snap"
